993128c5aa5285050865ec84591b5b46ede22949
Dao Anh Dung <dao.anh.dung@sun-asterisk.com>
2023-05-25 22:31:40 +0700

    Init project

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"go-base-gen/cmd"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+var (
+	version string = "v1.0.0"
+)
+
+func main() {
+	cli.VersionFlag = &cli.BoolFlag{
+		Name:    "version",
+		Aliases: []string{"v"},
+		Usage:   "print only the version",
+	}
+
+	project := cmd.NewProject()
+
+	app := &cli.App{
+		Name:    "go-base-gen",
+		Suggest: true,
+		Version: version,
+		Usage:   "Use this tool to generate base code",
+		Commands: []*cli.Command{
+			project,
+		},
+		Action: func(ctx *cli.Context) error {
+			if err := cli.ShowAppHelp(ctx); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
